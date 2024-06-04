df8346a0b6fc62ff843f7e392fe1b09f1d0d2d91
Areej Omar Abumuhfouz <areejabumahfouz@gmail.com>
2024-06-04 15:51:35 +0300

    edited by areej

--- app.js
+++ app.js
@@ -1,17 +1,75 @@
-let name=window.prompt("Enter your Name:","");
-let gender=window.prompt("Enter your Gender:","Male Or Female");
-console.log (gender);
+// let name=window.prompt("Enter your Name:","");
+// let gender=window.prompt("Enter your Gender:","Male Or Female");
+// console.log (gender);
 
-if (gender == "Male"){
-   window.alert("Mr. " +name);
-}
-else if(gender == "Female"){
-    window.alert("Miss. " + name);
-}
-else{
-    window.alert("   ");
-}
-window.alert("Do you want to order Donut or Coffee or Ice Cream or Bakery?? " );
-let order=window.prompt("write name of your order","Donut or Coffee or Ice Cream or Bakery");
-window.alert("your order is getting prapar. " );
-console.log(name+order)
+// if (gender == "Male"){
+//    window.alert("Mr. " +name);
+// }
+// else if(gender == "Female"){
+//     window.alert("Miss. " + name);
+// }
+// else{
+//     //window.prompt("Entefffffffffffffff:" +"");
+// while(gender!="Male" || gender!="Female"){
+//     window.prompt("Enter your Gender:" +"Male Or Female");
+//     if(gender == "Male")
+//     {
+//         window.alert("Mr. " +name);
+//      }
+//      else (gender == "Female")
+//      {
+//          window.alert("Miss. " + name);
+//      }
+// }
+// }
+
+// window.alert("Do you want to order Donut or Coffee or Ice Cream or Bakery?? " );
+// let order=window.prompt("write name of your order","Donut or Coffee or Ice Cream or Bakery");
+// window.alert("your order is getting prapar. " );
+// console.log(name+order)
+
+// const array=[];
+// array.push(name);
+// array.push(gender);
+// array.push(order);
+
+// for(let j=0;j<=array.length;j++){
+//     console.log(array[j]);
+// }
+
+
+// Question 7
+
+// for(let i=0;i<=5;i++){
+//     window.alert(i);
+//     }
+
+
+// // Question 8
+// const num=[];
+// for(let i=0;i<=5;i++){
+//      num.push(i+" ");
+//         }
+// window.alert(num);
+    
+// Question 9
+// const num=[];
+// for(let i=0;i<=20;i+=3){
+//      num.push(i+" ");
+//         }
+// window.alert(num);
+
+// Question 10
+
+// let NuM=window.prompt("Enter number from 1 to 100","");
+// while(NuM<0 || NuM>100){
+//     window.alert("Error number you should enter number from 0 to 100!!!");
+// }
+
+// Question 11
+// let NuM=window.prompt("Enter number from 1 to 100","");
+// let n=0;
+// for(let i=0;i<=NuM;i++){
+//     n=n+i;
+// }
+// window.alert(n);
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundColor:{
+      "orange":"#F6891F",
+      "white":"#FEFEFF",
+    },
+    colors:{
+      "white":"#FEFEFF",
+    }
+  },
+  plugins: [],
+}
+
